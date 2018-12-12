de8494f8a115aef337d73106fb8dba5dbd09e404
Matthew <42585487+Matthew-Denholm@users.noreply.github.com>
2018-12-12 13:40:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
+
+
 
 var indexRouter = require('./routes/index');
 var catalogRouter = require('./routes/catalog');
@@ -25,6 +29,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Router
